2b7040f49f361d350d821783422d6bc13ce66844
Gonçalo Heleno <goncalocheleno@atomp.eu>
2024-12-16 17:56:15 +0100

    fix: fix outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,6 @@
 output "virtual_machine" {
   value = {
-    name       = resource.azurerm_virtual_machine.ubuntu_vm.name
-    id         = resource.azurerm_virtual_machine.ubuntu_vm.id
+    name       = resource.azurerm_linux_virtual_machine.ubuntu_vm.name
     public_ip  = resource.azurerm_public_ip.public_ip.ip_address
     public_dns = resource.azurerm_public_ip.public_ip.fqdn
   }

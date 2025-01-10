b6f404a2c52508ee9760439d69eed6494feee6cf
PedroAS7 <pedrocas-pt@hotmail.com>
2025-01-10 11:23:24 +0100

    feat: Initial classes and dummy endpoint

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,8 +1,31 @@
 package ch.heigvd.dai;
+import ch.heigvd.dai.cache.Cache;
+import ch.heigvd.dai.db.Database;
+import ch.heigvd.dai.endpoints.*;
+import io.javalin.Javalin;
+import io.javalin.config.Key;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    Database db = new Database();
+    Cache cache = new Cache();
+
+    Dummy dummy = new Dummy();
+
+    var app = Javalin.create(config -> {
+      config.appData(new Key<>("database"), db);
+      config.appData(new Key<>("cache"), cache);
+
+          config.router.apiBuilder(() -> {
+            path("/", () -> {
+              get(dummy::DummyEndpoint);
+            });
+          });
+        });
+
+    app.start(7070);
   }
 }
---
+++ src/main/java/ch/heigvd/dai/endpoints/Dummy.java
@@ -0,0 +1,39 @@
+/*
+ * gpg-keyserver - a web app to store user's GPG keys
+ * Copyright (C) 2024 Pedro Alves da Silva, Gonçalo Carvalheiro Heleno
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.heigvd.dai.endpoints;
+import ch.heigvd.dai.cache.Cache;
+import ch.heigvd.dai.db.Database;
+import io.javalin.config.Key;
+import io.javalin.http.*;
+import org.jetbrains.annotations.NotNull;
+
+public class Dummy {
+  public Dummy()
+  {
+
+  }
+
+  public void DummyEndpoint(@NotNull Context ctx) throws Exception
+  {
+    final Database database = ctx.appData(new Key<>("database"));
+    final Cache cache = ctx.appData(new Key<>("cache"));
+
+    ctx.result("404 - Found");
+  }
+}
